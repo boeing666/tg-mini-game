9a7570baa6312be4db84fe9f9c2eef111745e05e
Boeing666 <linbergh.dd@gmail.com>
2025-05-03 19:13:45 +0500

    add STATIC_FILES_BASE_URL env

--- server/api/img/[hash].ts
+++ server/api/img/[hash].ts
@@ -3,7 +3,7 @@ import { readFile } from 'fs/promises'
 import { resolve } from 'path'
 import { decodeImageHash } from '~/utils/imageHash'
 
-const IMAGE_DIR = 'public/images'
+const IMAGE_URL = process.env.STATIC_FILES_BASE_URL as string;
 
 export default defineEventHandler(async (event) => {
   const hash = getRouterParam(event, 'hash')
@@ -16,12 +16,19 @@ export default defineEventHandler(async (event) => {
     return sendError(event, createError({ statusCode: 404, statusMessage: 'Image not found' }))
   }
 
-  const filepath = resolve(IMAGE_DIR, filename)
+  const imageUrl = `${IMAGE_URL}/${filename}`
+
   try {
-    const file = await readFile(filepath)
+    const response = await fetch(imageUrl)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image: ${response.statusText}`)
+    }
+    const buffer = await response.arrayBuffer()
+    const file = Buffer.from(buffer)
+
     event.node.res.setHeader('Content-Type', 'image/svg+xml')
-    return file
+    event.node.res.end(file)
   } catch (e) {
-    return sendError(event, createError({ statusCode: 500, statusMessage: 'Error reading image' }))
+    return sendError(event, createError({ statusCode: 500, statusMessage: 'Error fetching or processing image' }))
   }
 })
